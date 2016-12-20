8baa7e41be4c1d5ea33426a5e5cd7175dcd19e43
bilel68@gmail.com <bilel68@gmail.com>
2016-12-20 16:16:58 +0000

    Jeux

---
+++ js/hero.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+  heroList = [
+    {
+      nom: "Jean-louis",
+      classe: "Guerrier",
+      pv: 10,
+      pm: 10,
+      status: 'alive',
+      img: "src/portraits/heros1l.bmp"
+    },
+    {
+      nom: "Bruno",
+      classe: "Mage",
+      pv: 10,
+      pm: 30,
+      status: 'alive',
+      img: "src/portraits/heros2l.bmp"
+    },
+    {
+      nom: "Lucas",
+      classe: "Paladin",
+      pv: 10,
+      pm: 15,
+      status: 'alive',
+      img: "src/portraits/heros3l.bmp"
+    },
+    {
+      nom: "Shaka",
+      classe: "Druide",
+      pv: 10,
+      pm: 20,
+      status: 'alive',
+      img: "src/portraits/heros4l.bmp"
+    }
+  ];
+});
